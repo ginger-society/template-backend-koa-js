82490a6bc9ead56f6bed80108c345cae06f6af93
Ginger Society Admin <admin@gingersociety.org>
2024-07-02 17:13:51 +0530

    chore: test cases fixes

--- __test__/first.test.ts
+++ __test__/first.test.ts
@@ -1,11 +1,6 @@
 import { cache, server } from '@/app'
 import db, { connection } from '@/db'
-import {
-	AdministratorEntity,
-	Many2ManyTestEntity,
-	TenantEntity,
-	TestEntity
-} from '@/models/i_am_1'
+import { StudentEntity } from '@/models'
 // import { classNames } from '@pkyad/jslib1'
 import request from 'supertest'
 
@@ -17,7 +12,7 @@ beforeAll(async () => {
 
 afterAll(async () => {
 	// close the server after each test
-	; (await server).close()
+	;(await server).close()
 	connection.disconnect()
 	cache.disconnect()
 	// console.log('server closed!')
@@ -35,8 +30,8 @@ describe('first suite', () => {
 	it('checks if unauthorized', async () => {
 		const response = await app.get('/')
 
-		const allUsers = await db.find(TestEntity)
-		expect(allUsers.length).toBeGreaterThan(0)
+		const allStudents = await db.find(StudentEntity)
+		expect(allStudents.length).toBeGreaterThan(0)
 		expect(response.status).toBe(401)
 	})
 	it('checks response from cache', async () => {
@@ -46,58 +41,39 @@ describe('first suite', () => {
 		expect(response.body).toMatchObject({ key: cacheData })
 	})
 	it('checks if tenants can be created with adminstrator', async () => {
-		const tenant = new TenantEntity()
-		tenant.name = 'second tenant 12'
-		tenant.is_active = true
-		tenant.expiry_date = new Date().toDateString()
+		const student = new StudentEntity()
+		student.name = 'second tenant 12'
+		student.has_cab_service = true
+		student.updated_at = new Date().toDateString()
+		student.created_at = new Date()
+		student.roll_number = 'ROLL01'
+		student.on_scholarship = false
+		student.address = 'Address text'
 
-		await db.save(tenant)
-
-		const admin = new AdministratorEntity()
-		admin.email = 'test11@gmail.com'
-		admin.is_active = true
-		admin.is_admin = true
-		admin.is_staff = true
-		admin.last_login = new Date()
-		admin.mobile = '9876543210'
-		admin.tenant = tenant
-		admin.name = 'User Kumar'
-		admin.next_password_change_due = new Date().toDateString()
-		admin.password = 'encryptedStr'
-		admin.password_change_required = true
-
-		await db.save(admin)
-
-		// const allTenants = await db.find(TenantEntity)
-		// const allAdmin = await db.find(AdministratorEntity, {
-		// 	relations: {
-		// 		tenant: true
-		// 	}
-		// })
-		// console.log(allTenants, allAdmin)
+		await db.save(student)
 	})
 	it('gets admins from a tenant', async () => {
-		const allTenants = await db.find(TenantEntity, {
+		const allStudents = await db.find(StudentEntity, {
 			relations: {
-				users: true
+				courses: true
 			}
 		})
-		const lastTenant = allTenants[allTenants.length - 1]
-		expect(lastTenant).toBeDefined()
+		const lastStudent = allStudents[allStudents.length - 1]
+		expect(lastStudent).toBeDefined()
 	})
-	it('laads many to many fields', async () => {
-		const m2mTestModels = await db.find(Many2ManyTestEntity, {
-			relations: {
-				testModels: true,
-				crossAppModel: true
-			}
-		})
-		const testModels = await db.find(TestEntity, {
-			relations: {
-				main_models: true
-			}
-		})
-		// eslint-disable-next-line no-console
-		console.log({ m2mTestModels, testModels })
-	})
+	// it('laads many to many fields', async () => {
+	// 	const m2mTestModels = await db.find(Many2ManyTestEntity, {
+	// 		relations: {
+	// 			testModels: true,
+	// 			crossAppModel: true
+	// 		}
+	// 	})
+	// 	const testModels = await db.find(TestEntity, {
+	// 		relations: {
+	// 			main_models: true
+	// 		}
+	// 	})
+	// 	// eslint-disable-next-line no-console
+	// 	console.log({ m2mTestModels, testModels })
+	// })
 })
